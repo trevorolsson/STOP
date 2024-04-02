ac53e41841cf71ed4ecb9fef68a1fd9a3824aa81
Trevor Olsson <86252196+trevorolsson@users.noreply.github.com>
2024-04-02 18:03:13 -0500

    Update SpatioTemporalOverlapPositioner.py

--- SpatioTemporalOverlapPositioner.py
+++ SpatioTemporalOverlapPositioner.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*- 
- 
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
-
 import wx
 import wx.lib.activex
 import numpy as np
